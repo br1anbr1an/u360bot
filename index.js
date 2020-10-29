c87550b71e0cff90cfb678a24fb37ab55e2c9e10
b.monroymejia <b.monroymejia@tcs.com>
2020-10-29 14:35:04 -0600

    adding call capability

--- index.js
+++ index.js
@@ -85,6 +85,207 @@ server.on('upgrade', (req, socket, head) => {
     });
 });
 
+// Listen for incoming calls.
+server.post('/api/calls', (req, res) => {
+
+    // ANSWER CALL
+    let CommID = req.body.value[0].resourceData.id;
+    console.log('INCOMING CALL...')
+    res.send(202);
+    answerCall(CommID);
+
+});
+
+// Listen for incoming calls notifications.
+server.post('/api/calls/hub', (req, res) => {
+
+    res.send(202);
+
+    let CommID = req.body.value[0].resourceUrl.split("/")[3];
+
+    if ((req.body.value[0].resourceData).hasOwnProperty('state')) {
+
+        switch(req.body.value[0].resourceData.state) {
+        
+            // CALL SENDO ESTABELECIDA
+            case 'establishing':
+            console.log('The call is being established... :D');
+            break;
+    
+            // CALL ESTABELECIDA
+            case 'established':
+
+            // CALL ESTABLISHED, PLAY MEDIA PROMPT
+            if ((req.body.value[0].resourceData).hasOwnProperty('mediaState')) {
+                console.log('Call Established. Press 1 to record an audio clip (and * after the recording) and 1 to hang up! :)');
+                playPrompt(CommID);
+                subscribeToTone(CommID);
+            }
+
+            // ACT ON TONE RECEIVED
+            if ((req.body.value[0].resourceData).hasOwnProperty('toneInfo')) {
+                let dialedTone = (req.body.value[0].resourceData.toneInfo.tone).substr(-1);             
+                console.log('You pressed the tone ' + dialedTone);
+                runToneLogic(CommID, dialedTone);
+            }
+            break;
+
+            // CALL BEING TERMINATED
+            case 'terminating':
+            console.log('The call is being terminated... :(');
+            break;
+    
+            // CALL TERMINADA
+            case 'terminated':
+            console.log('Call Terminated :/');
+            break;
+    
+            // DEFAULT ROUTE
+            default:
+            console.log('Ops... Leverage ngrok inspector for additional fields');
+            console.log(req.body.value[0].resourceData)
+            console.log('____________________________________________________________________');
+        }
+
+    } else {
+
+    }
+    
+});
+
+function answerCall(CommID) {
+    let answergraphURL = 'https://graph.microsoft.com/beta/app/calls/' + CommID + '/answer';
+    console.log('This is the endpoint to answer: ' + answergraphURL);
+
+            request.post(
+                {
+                    url:answergraphURL,
+                    json: 
+                    {
+                        "callbackUri": process.env.ngrok + "/api/calls/hub",
+                        "acceptedModalities": [ "audio" ],
+                        "mediaConfig": {
+                            "@odata.type": "#microsoft.graph.serviceHostedMediaConfig"
+                        }
+                    }
+            }).auth(null, null, true, accessToken);
+}
+
+function playPrompt(CommID) {
+    
+    let playPromptgraphURL = 'https://graph.microsoft.com/beta/app/calls/' + CommID + '/playPrompt';
+        // PLAY PROMPT
+        request.post(
+            {
+                url:playPromptgraphURL,
+                json: 
+                {
+                    "clientContext": "playprompt-client-context",
+                    "prompts": [
+                        {
+                        "@odata.type": "#microsoft.graph.mediaPrompt",
+                        "mediaInfo": {
+                            "@odata.type": "#microsoft.graph.mediaInfo",
+                            "uri": process.env.playPromptURL,
+                            "resourceId": "2G6DE2D4-CD51-4309-8DAA-70768651088E"
+                        }
+                        }
+                    ],
+                    "loop": false
+                }
+        }).auth(null, null, true, accessToken), function(err,httpResponse,body)
+        {
+            if (err) {
+                console.log(err);
+            }
+            console(body);
+        };
+}
+
+function subscribeToTone(CommID) {
+    
+    let subscribeToTonegraphURL = 'https://graph.microsoft.com/beta/app/calls/' + CommID + '/subscribeToTone';
+        // PLAY PROMPT
+        request.post(
+            {
+                url:subscribeToTonegraphURL,
+                json: 
+                {
+                    "clientContext": "subscribing-to-tone-client-context",
+                }
+        }).auth(null, null, true, accessToken), function(err,httpResponse,body)
+        {
+                console('Successfully subsribed to tone :D');
+        };
+}
+
+function runToneLogic(CommID, dialedTone) {
+    
+    switch(dialedTone) {
+        
+        // RECORD AN AUDIO CLIP
+        case '1':
+        recordAudioClip(CommID);
+        console.log('Audio clip is being recorded, please leverage the ngrok inspector to retrieve the location and token to get the audio as this has not being implemented yet.');
+        break;
+
+        // JUST HANG UP
+        case '2':
+        hangUp(CommID)
+        console.log('Hanging up, bye! :)');
+        break;
+
+        // NOT MANAGED TONE
+        default:
+        console.log("THE TONE DIALED DOESN'T HAVE A HANDLE YET :/");
+        break;
+    }
+
+    let subscribeToTonegraphURL = 'https://graph.microsoft.com/beta/app/calls/' + CommID + '/subscribeToTone';
+        // PLAY PROMPT
+        request.post(
+            {
+                url:subscribeToTonegraphURL,
+                json: 
+                {
+                    "clientContext": "subscribing-to-tone-client-context",
+                }
+        }).auth(null, null, true, accessToken), function(err,httpResponse,body)
+        {
+                console('Successfully subsribed to tone :D');
+    };
+}
+
+function recordAudioClip(CommID) {
+    
+    let recordAudioClipGraphURL = 'https://graph.microsoft.com/beta/app/calls/' + CommID + '/record';
+        // PLAY PROMPT
+        console.log('To finish recording, press * ')
+        request.post(
+            {
+                url:recordAudioClipGraphURL,
+                json: 
+                {
+                    "bargeInAllowed": true,
+                    "clientContext": "record-audio-clip-context",
+                    "maxRecordDurationInSeconds": 10,
+                    "initialSilenceTimeoutInSeconds": 5,
+                    "maxSilenceTimeoutInSeconds": 2,
+                    "playBeep": true,
+                    "stopTones": [ "*" ]
+                }
+        }).auth(null, null, true, accessToken);
+}
+
+function hangUp(CommID) {
+    let hangUpgraphURL = 'https://graph.microsoft.com/beta/app/calls/' + CommID;
+        // HANG UP
+        request.delete(
+            {
+                url:hangUpgraphURL
+        }).auth(null, null, true, accessToken);
+}
+
 // SIG // Begin signature block
 // SIG // MIInOQYJKoZIhvcNAQcCoIInKjCCJyYCAQExDzANBglg
 // SIG // hkgBZQMEAgEFADB3BgorBgEEAYI3AgEEoGkwZzAyBgor
